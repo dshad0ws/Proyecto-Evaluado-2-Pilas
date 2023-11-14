fde0fb0a69ec983003e7763009cde6c29273e6c7
Daniel <Daniel@DANIEL>
2023-11-14 13:18:31 -0400

    Agregar archivos de proyecto.

---
+++ Proyecto Evaluado 2 Pilas/Pilas.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Evaluado_2_Pilas
+{
+    internal class Pilas
+    {
+        #region
+        protected const int max = 5;
+        string[] Elementos;
+        int tope;
+        #endregion
+
+        public Pilas()
+        {
+            tope = -1;
+            Elementos = new string[max];
+
+        }
+
+        private bool vacia()
+        {
+            return (tope == -1);
+        }
+
+        private bool llena()
+        {
+            return (tope == max - 1);
+        }
+
+        public void Push(string Dato)
+        {
+            if (llena())
+            {
+            }
+            else
+            {
+                Elementos[++tope] = Dato;
+            }
+        }
+
+        public string Pop()
+        {
+            if (vacia())
+            {
+                return null;
+            }
+            else
+            {
+                string D;
+                D = Elementos[tope];
+                tope--;
+                return D;
+            }
+        }
+
+        public string Poptope()
+        {
+            if (vacia())
+            {
+                return null;
+            }
+            else
+            {
+                string D;
+                D = Elementos[tope];
+                return D;
+            }
+        }
+
+        public void Imprimir()
+        {
+            if (vacia())
+            {
+            }
+
+            else
+            {
+                string d;
+                Pilas Pa = new Pilas();
+                while (!vacia())
+                {
+                    d = Pop();
+                    Console.WriteLine(d);
+                    Pa.Push(d);
+                }
+
+                while (!Pa.vacia())
+                {
+                    Push(Pa.Pop());
+                }
+            }
+
+        }
+
+        public int Count()
+        {
+            return ++tope;
+
+        }
+
+        public void Clear()
+        {
+            tope = -1;
+        }
+
+    }
+}
---
+++ Proyecto Evaluado 2 Pilas/Program.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Proyecto_Evaluado_2_Pilas
+{
+    internal class Program
+    {
+        static int calcular(string op1, string op2, string oper)
+        {
+            int r = 0;
+            if (oper == "+") { r = int.Parse(op1) + int.Parse(op2); }
+            else if (oper == "-") { r = int.Parse(op2) - int.Parse(op1); }
+            else if (oper == "*") { r = int.Parse(op1) * int.Parse(op2); }
+            else if (oper == "/")
+            {
+                if (int.Parse(op2) != 0)
+                {
+                    r = int.Parse(op2) / int.Parse(op1);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    r = 0;
+                }
+            }
+            else
+            {
+                r = Convert.ToInt32(Math.Pow(double.Parse(op1), double.Parse(op2)));
+            }
+            return r;
+        }
+            static void evaluando(string ex)
+        {
+            bool Error = false;
+            int cp = 0;
+            Pilas p1 = new Pilas();
+            string op1 = null;
+            string op2 = null;
+            string oper = null;
+            string expf = null;
+            for (int i = 0; i < ex.Length; i++)
+            {
+                if (ex[i] == '(')
+                {
+                    cp++;
+                }
+                else if (ex[i] == ')')
+                {
+                    op2 = p1.Pop();
+                    oper = p1.Pop();
+                    op1 = p1.Pop();
+                    if (op1 != null && op2 != null && oper != null)
+                    {
+                        if ((oper == "+" || oper == "-" || oper == "*" || oper == "/" || oper == "*"))
+                        {
+
+                            expf = Convert.ToString(calcular(op1, op2, oper));
+                            p1.Push(expf);
+                        }
+                    }
+                    else
+                    {
+                        Error = true;
+                    }
+                    cp--;
+                    op2 = "";
+                    oper = "";
+                    op1 = "";
+
+                }
+                else if (cp > 0)
+                {
+                    p1.Push(Convert.ToString(ex[i]));
+
+                }
+            }
+
+            if (Error)
+            {
+                Console.WriteLine("Mano eso ta mal escrito");
+            }
+            else
+            {
+                Console.WriteLine(p1.Pop());
+            }
+
+            Console.ReadKey();
+        }
+            static void transformando(string ex)
+            {
+            bool Error = false;
+            int cp = 0;
+                Pilas p1 = new Pilas();
+                string op1 = null;
+                string op2 = null;
+                string oper = null;
+                string expf = null;
+                for (int i = 0; i < ex.Length; i++)
+                {
+                if (ex[i]=='(')
+                {
+                    cp++;
+                }
+                    else if (ex[i] == ')')
+                    {
+                        op2 = p1.Pop();
+                        oper = p1.Pop();
+                        op1 = p1.Pop();
+                        if(op1 != null && op2 != null && oper != null)
+                        {
+                        if((oper == "+" || oper == "-" || oper == "*" || oper == "/" || oper == "*"))
+                        {
+                            expf = op1 + op2 + oper;
+                            p1.Push(expf);
+                        }
+                            
+                        }
+                    else
+                    {
+                        Error = true;
+                    }
+                    cp--;
+                        op2 = "";
+                        oper = "";
+                        op1 = "";
+
+                }
+                    else if(cp>0)
+                    {
+                        p1.Push(Convert.ToString(ex[i]));
+
+                    }
+                }
+
+            if (Error)
+            {
+                Console.WriteLine("Mano eso ta mal escrito");
+            }
+            else
+            {
+                Console.WriteLine(p1.Pop());
+            }
+            
+                Console.ReadKey();
+            }
+            static bool validarexpos(string ex)//funcion para validar la expresion tomando en cuenta la parametros
+            {
+                int pa = 0;
+                int pc = 0;
+                int op = 0;
+                int ope = 0;
+                for (int i = 0; i < ex.Length; i++)
+                {
+                    if (ex[i] == '(')
+                    {
+                        pa++;
+                    }
+                    else if (ex[i] == ')')
+                    {
+                        pc++;
+                    }
+                    else if ((ex[i] == '+' || ex[i] == '-' || ex[i] == '*' || ex[i] == '/' || ex[i] == '^'))
+                    {
+                        op++;
+
+                    }
+                    else if ((ex[i] >= 65 && ex[i] <= 90) || (ex[i] >= 97 && ex[i] <= 122))
+                    {
+                        ope++;
+
+                    }
+
+
+                }
+                if (pa == pc && pa >= 1)
+                {
+                    if (ope == op + 1)
+                    {
+                        if (op >= 1 && op == pa)
+                        {
+                            if(pa+pc+op+ope==ex.Length) return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            static bool validarex(string ex)
+            {
+                int pa = 0;
+                int pc = 0;
+                int op = 0;
+                int ope = 0;
+                for (int i = 0; i < ex.Length; i++)
+                {
+                    if (ex[i] == '(')
+                    {
+                        pa++;
+                    }
+                    else if (ex[i] == ')')
+                    {
+                        pc++;
+                    }
+                    else if (ex[i] == '+' || ex[i] == '-' || ex[i] == '*' || ex[i] == '/' || ex[i] == '^')
+                    {
+                        op++;
+
+                    }
+                    else if (ex[i] >= 48 && ex[i] <= 57)
+                    {
+                        ope++;
+
+                    }
+
+                }
+                if (pa == pc && pa >= 1)
+                {
+                    if (ope == op + 1)
+                    {
+                        if (op >= 1 && op == pa)
+                        {
+                        if (pa + pc + op + ope == ex.Length) return true; 
+                        }
+                    }
+                }
+                return false;
+            }
+            static void menu() //menu de opciones
+            {
+                int op = 1;
+                string exp = "";
+                bool error;
+                string msj = "";
+
+                do
+                {//creamos las opciones dentro de un do while para evitar alguna opcion no permitida 
+                    Console.Clear();
+                    error = false;
+                    Console.WriteLine("Menu de opciones");
+                    Console.WriteLine("<1> Transformar Expresion");
+                    Console.WriteLine("<2> Evaluar Expresion");
+                    Console.WriteLine("<3> Salir");
+                    try
+                    {
+                        Console.Write("Opcion: ");
+                        op = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        op = -1;
+                        msj = ex.Message;
+                        error = true;
+                    }
+
+                    if (op == 3)
+                    {
+                        Console.Clear();
+                        Console.Write("Hasta la proximaaa");
+                        Console.ReadKey();
+                    }
+                    else if (op == 1)
+                    {
+
+                        Console.Clear();
+                        Console.WriteLine("Ingrese la expresion que desea transformar");
+                        exp = Console.ReadLine();
+                        if (validarexpos(exp) == true)
+                        {
+                            transformando(exp);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mano eso ta malo. No cumple con los parametros establecidos.");
+                        }
+                        Console.ReadKey();
+
+
+                    }
+                    else if (op == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Por favor Ingrese la expresion");
+                    exp= Console.ReadLine();
+                        if (validarex(exp) == true)
+                        {
+                            evaluando(exp);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mano eso ta malo. No cumple con los parametros establecidos");
+                        }
+                        Console.ReadKey();
+
+                    }
+                    else if (error == false)
+                    {
+                        msj = "Valor invalido";
+                        error = true;
+                    }
+                    if (error)
+                    {
+                        Console.WriteLine("Error: " + msj);
+                        op = -1;
+                        Console.ReadKey();
+                    }
+                } while (op != 3);
+            }
+            static void Main(string[] args)
+            {
+                menu();
+           }
+    }
+}
